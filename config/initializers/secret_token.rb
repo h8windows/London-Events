f60e3f6d872c48dc20b1d404c6cb57c014c92237
Chris <h8windows@gmail.com>
2012-03-02 08:28:21 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Londonevents::Application.config.secret_token = '9d0859b8e659b88e2f579902e876b074a6fa61a4d628ea6e9b1203ff70a7103d1764bca1bd5499717c5b1c984dc6ab47098046128fd27574b51fa568afc16bb5'
